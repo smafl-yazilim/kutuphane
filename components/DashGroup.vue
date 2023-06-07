e845983df5d153403684cbce0db67a4176f0ad74
Enes Genç <61084101+enxg@users.noreply.github.com>
2023-06-07 12:22:08 +0300

    fix: don't grow on grid

--- components/DashGroup.vue
+++ components/DashGroup.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="flex flex-col p-6 rounded-xl bg-white gap-3">
-        <slot />
+    <div>
+        <div class="flex flex-col p-6 rounded-xl bg-white gap-3">
+            <slot />
+        </div>
     </div>
 </template>
+<script setup lang="ts">
+</script>
