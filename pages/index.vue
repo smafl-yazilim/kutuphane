41a04b35795e4f36cec07fd834881a7f845786d3
Enes Genç <61084101+enxg@users.noreply.github.com>
2023-06-07 12:21:34 +0300

    feat: add Algolia logo to footer

--- pages/index.vue
+++ pages/index.vue
@@ -41,26 +41,29 @@ const randomBook = books[Math.floor(Math.random() * books.length)];
       </div>
     </div>
     <div class="relative flex flex-col items-center justify-center h-[90vh] w-full">
-      <div class="absolute left-0 top-0 z-0 bg-gradient-to-t from-slate-950 to-transparant w-full h-[90vh]"/>
+      <div class="absolute left-0 top-0 z-0 bg-gradient-to-t from-slate-950 w-full h-[90vh]"/>
       <div class="flex flex-col w-2/5 text-gray-100 text-center drop-shadow-2xl gap-2">
         <h1 class="text-6xl font-bold w-full">Şehit Münir Alkan Fen Lisesi Kütüphanesi</h1>
         <h2 class="text-lg w-full" >Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
       </div>
       <form class="max-w-screen-xl mx-auto w-full">
-        <div class="flex md:h-20 h-14 w-full flex-row bg-gray-100 bg-opacity-60 backdrop-blur-lg my-5 rounded-xl z-0">
+        <div class="flex md:h-20 h-14 w-full flex-row bg-gray-100 bg-opacity-80 backdrop-blur-lg my-5 rounded-xl">
           <input
             type="text"
-            class="mr-auto h-full w-full flex bg-transparent outline-none pl-6 pr-4 py-2 md:text-3xl text-2xl text-slate-800 placeholder:text-slate-600"
+            class="mr-auto h-full w-full flex bg-transparent outline-none pl-6 pr-4 py-2 md:text-3xl text-2xl text-black border-0 placeholder:text-black/75"
             :placeholder="randomBook"
           />
-          <button class="flex px-5"><icon class="md:h-8 h-5 px-1 mx-auto my-auto w-auto text-black/50" name="uil:search"/></button>
+          <button class="flex px-5 group"><icon class="md:h-8 h-5 px-1 mx-auto my-auto w-auto text-black/50 group-hover:text-white/50 transition duration-200" name="uil:search"/></button>
         </div>
       </form>
     </div>
     <div class="absolute bottom-0 left-0 w-full">
       <div class="max-w-screen-xl mx-auto py-6 w-full flex flex-row items-center justify-between text-gray-100">
-        <span class="text-gray-200"><a href="https://enesgenc.dev" target="_blank" class="font-semibold text-gray-100 hover:text-blue-500 duration-150 mr-auto">Enes Genç</a> tarafından yapılmıştır.</span>
-        <div>
+        <div class="flex flex-grow basis-0">
+            <span class="text-gray-200"><a href="https://enesgenc.dev" target="_blank" class="font-semibold text-gray-100 hover:text-blue-500 duration-150 mr-auto">Enes Genç</a> tarafından yapılmıştır.</span>
+        </div>
+        <Sponsors class="" />
+        <div class="flex flex-grow basis-0">
           <a class="ml-auto text-2xl" href="https://github.com/smafl-yazilim/kutuphane"><icon name="uil:github"/></a>
         </div>
       </div>
